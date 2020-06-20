a5eaf942a6e2079ad30900b4172118af54acf31a
Igor Wysocki <lgorvysocky@gmail.com>
2020-06-20 22:31:52 +0200

    add React to app

---
+++ dev/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Widget } from 'react-chat-widget';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+            <Widget />
+            </div>
+        );
+    }
+}
+
+export default App;
