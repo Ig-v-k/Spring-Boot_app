2801ce2f0a842a2d0d2b6c211de5993dec3e204c
Igor Wysocki <lgorvysocky@gmail.com>
2020-06-23 16:16:55 +0200

    sql comment

--- src/main/resources/db/migration/V2__Add_admin.sql
+++ src/main/resources/db/migration/V2__Add_admin.sql
@@ -17,12 +17,3 @@ insert into usr (id,
 insert into user_role (user_id,
                        roles) values (1, 'USER'),
                                      (1, 'ADMIN');
-
-# For to the encrypt data,
-# someone people say that need to use a api,
-# between front-end <-> db, where a role(api),
-# will be check validation and pass to continue.
-
-# Or, is a standard function, to use, in the MySQL how -
-# AES_ENCRYPT('data', @key)
-# AES_DECRYPT(@encrypted, @key)
